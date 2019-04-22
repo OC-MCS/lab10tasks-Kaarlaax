6cb02c7bd101097583804fa95f66654210bf191d
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 06:05:10 -0500

    Update Employee.h

--- Employee/Employee.h
+++ Employee/Employee.h
@@ -1,7 +1,20 @@
 #pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
 class Employee
 {
+	string name;
+	string number;
+	string hireDate;
+	
 public:
-	Employee();
+	Employee(string, string, string);
+	
+	string getName();
+	string getNumber();
+	string getHireDate();
 };
 
