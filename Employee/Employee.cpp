19397f0045d04a8681e89e8a6e8815194344d9b5
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 06:02:23 -0500

    Update Employee.cpp

--- Employee/Employee.cpp
+++ Employee/Employee.cpp
@@ -1,9 +1,30 @@
 #include "Employee.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
 
 
-
-Employee::Employee()
+Employee::Employee(string n, string num, string date)
 {
+  name = n;
+  number = num;
+  hireDate = date;
+}
+
+string Emploee::getName()
+{
+  neturn name;
+}
+
+string Employee::getNumber()
+{
+  return number;
+}
+
+string Employee::getHireDate()
+{
+  return hireDate;
 }
 
 
