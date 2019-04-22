cb637f236182ee612cca212137ec43dba5c269d1
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 06:45:27 -0500

    Update Odometer.h

--- Instrument/Odometer.h
+++ Instrument/Odometer.h
@@ -1,7 +1,15 @@
 #pragma once
+#include <iostream>
+#include "FuelGauge.h"
+
+using namespace std;
+
 class Odometer
 {
+    int milage;
+    
 public:
 	Odometer();
+    int getMilage();
+    void addMile(FuelGauge &fuelInst);
 };
-
