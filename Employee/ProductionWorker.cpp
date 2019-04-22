e8de1b61e3f67a1429a60450dbdeffa2d96374f5
Kaarlaax <47810959+Kaarlaax@users.noreply.github.com>
2019-04-22 06:17:28 -0500

    Update ProductionWorker.cpp

--- Employee/ProductionWorker.cpp
+++ Employee/ProductionWorker.cpp
@@ -1,9 +1,29 @@
 #include "ProductionWorker.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
-
-ProductionWorker::ProductionWorker()
+int ProductionWorker::getShift()
 {
+  return shift;
+}
+
+double ProductionWorker::getHourlyPay()
+{
+  return hourlyPay;
+}
+
+ProductionWorker::ProductionWorker(int shiftType, double pay, string n, string num, string date): Employee(n,num,date)
+{
+  shift = shiftType;
+  hourlyPay = pay;
+}
+
+ProductionWorker::ProductionWorker(int shiftType, double pay, Employee E): Employee (E.getName(),E.getNumber(),E.getHireDate())
+{
+  shift = shiftType;
+  hourlyPay = pay;
 }
 
 
